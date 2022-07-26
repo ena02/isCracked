475377fbe5da58486e1cf88f1b284fc1a344573a
ena <erkembekov.nurzhan@mail.ru>
2022-07-26 16:03:29 +0600

    Initial commit

---
+++ src/test/java/com/ena/iscracked/IsCrackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ena.iscracked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsCrackedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

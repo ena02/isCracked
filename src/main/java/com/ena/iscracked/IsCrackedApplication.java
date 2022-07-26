b74e250f0f651062ec5730b7c61982437dba0d8b
ena <erkembekov.nurzhan@mail.ru>
2022-07-26 15:32:52 +0600

    Initial commit

---
+++ src/main/java/com/ena/iscracked/IsCrackedApplication.java
@@ -0,0 +1,13 @@
+package com.ena.iscracked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsCrackedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsCrackedApplication.class, args);
+    }
+
+}
